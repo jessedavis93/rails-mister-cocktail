9c1e4f16be88743dfc5ba12bb38f272acc902c29
Jesse Davis <jdavis93@live.com>
2018-06-07 12:37:58 +0200

    cocktail routes set

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # get 'cocktails/index'
+  # get 'cocktails/new'
+  # get 'cocktails/create'
+  # get 'cocktails/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get   'cocktails',      to: 'cocktails#index'
+  get   'cocktails/new',   to: 'cocktails#new', as: :cocktail_new
+  post  'cocktails',      to: 'cocktails#create'
+  get   'cocktails/:id',  to: 'cocktails#show'
 end
