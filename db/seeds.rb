a228e24b8140910ffc642569fab36bcef774a190
Jesse Davis <jdavis93@live.com>
2018-06-07 12:21:00 +0200

    Seeded 10 ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+serialized = open(url).read
+list = JSON.parse(serialized)
+
+p "Sending astronauts to the outer depths of the solar system to find the answer..."
+p Ingredient.count
+
+10.times do
+  Ingredient.create(name: list["drinks"].sample["strIngredient1"])
+end
+
+p "Totes seeded"
+p Ingredient.count
